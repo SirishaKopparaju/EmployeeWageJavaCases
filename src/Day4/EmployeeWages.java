603020799ccf6b834ee821517f3744dc010dd549
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-23 18:59:46 +0530

    Added use case program 6

--- src/Day4/EmployeeWages.java
+++ src/Day4/EmployeeWages.java
@@ -1,36 +1,50 @@
 package Day4;
 
-public class EmployeeWages{
+public class EmployeeWages {
 
 	public static void main(String[]args) {
-
-		int IS_FULL_TIME=1;
-		int IS_PART_TIME=0;
-		int IS_ABSENT=2;
-
-		double empcheck=Math.floor(Math.random()*10)%3;
-		int Per_Hour_Salary=8;
-		int Total_Working_Days=20;
-		int Working_Hours=0;
-
-		int salary;
-		if (empcheck==IS_FULL_TIME) {
-			Working_Hours=8;		
-		}
+		int empcheck=(int) (Math.floor(Math.random()*10)%3);
+		int Working_Hours = 0;
+	int salary;
+  int Per_Hour_Salary = 20;
+  int Total_Working_Days=100;
+  int day = 0;
+  
 		
-		else if(empcheck==IS_PART_TIME) {
+		switch(empcheck)
+		{
+		case 0:
+			Working_Hours=8;
+			salary=Per_Hour_Salary*Working_Hours;
+			System.out.println("Employee is full time"+salary);
+
+
+			break;
+		case 1: 
 			Working_Hours=4;
-			}
-		else if(empcheck==IS_ABSENT) {
-			System.out.println("Employee is absent");
+			salary=Per_Hour_Salary*Working_Hours;
+			System.out.println("Employee is part time"+salary);
 			
+			break;
+		case 2:
+				Working_Hours=0;
+				salary=Per_Hour_Salary*Working_Hours;
+				System.out.println("Employee is absent"+salary);
+				break;
 		}
-		salary=Per_Hour_Salary*Working_Hours;
-	System.out.println("employee working "+salary);
-	Total_Working_Days=Total_Working_Days+salary;
-
-
+	
+			while (day <= 20 && Total_Working_Days <= 100 )
+		{
+				        
+			salary=Per_Hour_Salary*Working_Hours;
+				        if ( Total_Working_Days >= 100){
+				        		{
+				        	Total_Working_Days=Total_Working_Days+salary;
+				                break;
+	}	
+		
+				        }
+		}
+	}
 }
 		
-}
-
