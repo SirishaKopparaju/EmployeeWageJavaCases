76ad611cbb15373707ed22d401cfc44900735d4b
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-23 18:24:13 +0530

    ADDED use case 1

--- src/Day4/EmployeeWages.java
+++ src/Day4/EmployeeWages.java
@@ -1,5 +1,16 @@
 package Day4;
 
 public class EmployeeWages {
+	public static void main(String[]args) {
+		int IS_FULL_TIME=1;
+		double empcheck=Math.floor(Math.random()*10)%2;
+		if (empcheck==IS_FULL_TIME) {
+			System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is absent");
+			
+		}
+	}
 
 }
