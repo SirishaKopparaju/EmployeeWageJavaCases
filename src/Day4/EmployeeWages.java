6181f8bd930c4a70b9028cec9748bb029189b9bd
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-23 18:26:04 +0530

    ADDED CASE 2 PROGRAM

--- src/Day4/EmployeeWages.java
+++ src/Day4/EmployeeWages.java
@@ -4,13 +4,18 @@ public class EmployeeWages {
 	public static void main(String[]args) {
 		int IS_FULL_TIME=1;
 		double empcheck=Math.floor(Math.random()*10)%2;
+		int Per_Hour_Salary=20;
+		int Working_Hours=8;
+		int salary;
 		if (empcheck==IS_FULL_TIME) {
-			System.out.println("Employee is present");
+			salary=Per_Hour_Salary*Working_Hours;
+			System.out.println("Employee is present"+salary);
+			
 		}
 		else {
 			System.out.println("Employee is absent");
 			
 		}
 	}
-
+		
 }
